879e331611c6cc9e85f9b2c44612d040b0b35f75
BlackJack <122082106+lagtypqrvc@users.noreply.github.com>
2024-07-26 00:54:44 +0700

    Add files via upload

---
+++ nebula.sh
@@ -0,0 +1,39 @@
+cd /var/www/pterodactyl
+ php artisan down
+curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv 
+
+chmod -R 755 storage/* bootstrap/cache 
+ 
+composer install --no-dev --optimize-autoloader
+
+php artisan view:clear
+
+php artisan config:clear
+
+php artisan migrate --seed --force
+chown -R www-data:www-data /var/www/pterodactyl/*
+php artisan up
+        echo "Semua tema dan addon telah dihapus."
+        echo "PROSES PEMASANGAN NEBULA THEME"
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install -y nodejs
+npm i -g yarn
+cd /var/www/pterodactyl
+yarn
+yarn add cross-env
+apt install -y zip unzip git curl wget
+wget "$(curl -s https://api.github.com/repos/BlueprintFramework/framework/releases/latest | grep 'browser_download_url' | cut -d '"' -f 4)" -O release.zip
+mv release.zip var/www/pterodactyl/release.zip
+cd /var/www/pterodactyl
+unzip release.zip
+WEBUSER="www-data"; USERSHELL="/bin/bash"; PERMISSIONS="www-data:www-data";
+sed -i -E -e "s|WEBUSER=\"www-data\" #;|WEBUSER=\"$WEBUSER\" #;|g" -e "s|USERSHELL=\"/bin/bash\" #;|USERSHELL=\"$USERSHELL\" #;|g" -e "s|OWNERSHIP=\"www-data:www-data\" #;|OWNERSHIP=\"$PERMISSIONS\" #;|g" $FOLDER/blueprint.sh
+chmod +x blueprint.sh
+bash blueprint.sh
+cd /var/www/pterodactyl
+cd /var/www/pterodactyl && blueprint -install nebula
+
